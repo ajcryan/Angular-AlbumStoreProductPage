443f08d4b7ef668591595160eac04bc89a9910ea
jamie cryan <ajcryan@gmail.com>
2020-11-15 14:39:45 +1100

    finished part 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -11,8 +13,8 @@ private _albumUrl = "../assets/album.json";
 constructor(private _http: Http) {  }
 
 getAlbum(id: number){
-	return this._http.get(this._albumUrl)
-	.map((response) => response.json());
+	return Observable<Album>.this._http.get(this._albumUrl)
+	.map((response) => <Album>response.json());
 }
 
 }
